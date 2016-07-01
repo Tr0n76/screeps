d86bc47fdb0adc61e1624c29454736fc03a320da
Tr0n76 <richard@schraudt.de>
2016-07-01 10:07:05 +0200

    First basic steps. Creation of creeps. No defense.

---
+++ main.js
@@ -0,0 +1,77 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+
+module.exports.loop = function () {
+ 
+    create();
+   
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        var sources = creep.room.find(FIND_SOURCES);
+        var sourceForCreep = sources[0];
+        
+        // If there is more than one source every harvester is randomly assigned to one of the sources.
+        if (creep.memory.sourceId === undefined){
+            if (sources.length>1)    {
+                sourceToMine = sources[rand(0,sources.length-1)];
+               
+            }
+            creep.memory.sourceId = sourceToMine.id;
+            console.log(creep.name+"  role: "+creep.memory.role+ " source: "+creep.memory.sourceId);
+        }
+        sourceToMine = Game.getObjectById(creep.memory.sourceId);
+       
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep, sourceToMine);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep, sourceToMine);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep, sourceToMine);
+        }
+    }        
+}
+    var rand = function(min, max) {	
+        return Math.floor(Math.random() * (max - min + 1)) + min
+        
+    }
+
+    var create = function() {
+
+    	clean();
+
+    	if (createCreep('harvester', 6, [WORK, CARRY, CARRY, MOVE])){
+    		return;
+    	}
+    	if(createCreep('upgrader', 4, [WORK, WORK, CARRY, MOVE])){
+    		return;
+    	}
+    	if(createCreep('builder', 4, [WORK, WORK, CARRY,MOVE])){
+    		return;
+    	}
+               
+	}
+
+    var	createCreep = function(roleTyp, counter, work){
+        var exisitingCreeps = _.filter(Game.creeps, (creep) => creep.memory.role == roleTyp);
+        console.log(roleTyp + ": " + exisitingCreeps.length);
+
+        if(exisitingCreeps.length < counter) {
+            var newName = Game.spawns.Spawn1.createCreep(work, undefined, {role: roleTyp});
+            console.log('Spawning new '+roleTyp+' : ' + newName);
+            return true;
+        }
+        return false;
+	}
+
+    var	clean = function(){
+	for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+}
---
+++ role.upgrader.js
@@ -0,0 +1,27 @@
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function(creep, source) {
+
+        if(creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
+	    }
+	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.upgrading = true;
+	    }
+
+	    if(creep.memory.upgrading) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+        }
+	}
+};
+
+module.exports = roleUpgrader;
