418fcdfbaf370db9253e55ce5ad8185840d17742
Tr0n76 <richard@schraudt.de>
2016-07-01 11:50:26 +0200

    Fixed a bug and added condole logging.

--- role.guard.js
+++ role.guard.js
@@ -1,19 +1,23 @@
 var roleGuard = {
 
-    run: function(creep) {
-      
-    	var hostile = creep.room.findNearest(HOSTILE_CREEPS);
-    	if (hostile){
-    		if (!creep.pos.isNearTo(target)) {
-    			 creep.moveTo(target);
-    		}else{
-    			 creep.attack(target);
-    		}
-    	}else{
-    	      if (!creep.pos.isNearTo(spawn)) {
-    	            creep.moveTo(spawn);
-    	        }
-    	}
+	run : function(creep) {
+
+		var hostile = creep.room.findNearest(FIND_HOSTILE_CREEPS);
+		if (hostile) {
+			if (!creep.pos.isNearTo(target)) {
+				creep.moveTo(target);
+				console.log("Hostile creep from user " + hositle.owner.username
+						+ " spottet. Moving to engange.");
+			} else {
+				creep.attack(target);
+				console.log("Hostile creep from user " + hositle.owner.username
+						+ " spottet. Attakinge.");
+			}
+		} else {
+			if (!creep.pos.isNearTo(spawn)) {
+				creep.moveTo(spawn);
+			}
+		}
 	}
 }
 
