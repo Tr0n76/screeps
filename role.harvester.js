319290162415ffbd24da58e71825ac6d7e94d5a9
Tr0n76 <richard@schraudt.de>
2016-07-02 19:02:43 +0200

    Imporved Harvesters

--- role.harvester.js
+++ role.harvester.js
@@ -1,29 +1,49 @@
 var roleHarvester = {
 
     run: function(creep, source) {
-      
-
-	    if(creep.carry.energy < creep.carryCapacity) {
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
+    	
+    	harvestEnergy(creep)
+    	
+    	console.log("Harvester "+creep.name+" is harvesting "+creep.memory.harvesting);
+    	
+    	if (creep.memory.harvesting) { 
+    		if(creep.harvest(source) == ERR_NOT_IN_RANGE) {    	
+            creep.moveTo(source);
+            return;
+        }
+          
+    	var targets = getTargetsForEnergyTransfer();
+		
+        if(targets.length > 0) {
+        	
+            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0]);
             }
         }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            }
-        }
+    	}	           
 	}
+}
 
+function getTargetsForEnergyTransfer(){
+	return creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+            return (structure.structureType == STRUCTURE_EXTENSION ||
+                    structure.structureType == STRUCTURE_SPAWN ||
+                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+        }
+	});
+}
+
+function setHarvestFlagForCreep(creep){	
+	if (creep.carry.energy == 0){
+		 creep.memory.harvesting = true;
+		 return;
+	}
+		
+	if (creep.carry.energy == creep.carryCapacity){
+		 creep.memory.harvesting = false;
+		 return;
+	}	
 }
 
 module.exports = roleHarvester;
