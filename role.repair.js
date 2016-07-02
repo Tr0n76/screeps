bb272d1fbe9035ccd571ac1def963a3a79670932
Tr0n76 <richard@schraudt.de>
2016-07-02 19:40:37 +0200

    Now the repair creep is harvesting energy before he tries to repair something :-)

--- role.repair.js
+++ role.repair.js
@@ -1,19 +1,29 @@
 var roleRepair = {
 
 	run : function(creep) {
-
-		var structures = getTargetsForRepair(creep);
-
-		for (var i=0;i<structures.length;i++){
-			var item = structures[i];	
-			
-			console.log("Repair "+item.id +" hits="+item.hits+" max="+item.hitsMax +" Type "+item.structureType);
-			if (!creep.pos.isNearTo(item)) {
-				creep.moveTo(item);
-			} else {
-				creep.repair(item)
+		
+		setHarvestFlagForCreep(creep)
+    	
+    	if (creep.memory.harvesting) { 
+    		if(creep.harvest(source) == ERR_NOT_IN_RANGE) {  	    		
+    			creep.moveTo(source);    			
+    		}
+    	}else{    	
+			var structures = getTargetsForRepair(creep);
+	
+			console.log(structures.length);
+	
+			for (var i=0;i<structures.length;i++){
+				var item = structures[i];	
+				
+				console.log("Repair "+item.id +" hits="+item.hits+" max="+item.hitsMax +" Type "+item.structureType);
+				if (!creep.pos.isNearTo(item)) {
+					creep.moveTo(item);
+				} else {
+					creep.repair(item)
+				}
 			}
-		}								
+    	}
 	}
 }
 
@@ -23,9 +33,20 @@ function getTargetsForRepair(creep){
             return (structure.structureType === STRUCTURE_EXTENSION ||
                     structure.structureType === STRUCTURE_SPAWN ||
                     structure.structureType === STRUCTURE_TOWER ||
-                    structure.structureType ===  STRUCTURE_ROAD) && (structure.hits > structure.hitsMax);
+                    structure.structureType ===  STRUCTURE_ROAD) && (structure.hits < structure.hitsMax);
         }
 	});
+}
+
+function setHarvestFlagForCreep(creep){	
+    
+	if (creep.carry.energy === 0){
+		 creep.memory.harvesting = true;
+	}
+		
+	if (creep.carry.energy === creep.carryCapacity){
+		 creep.memory.harvesting = false;
+	}	
 }
 
 module.exports = roleRepair;
