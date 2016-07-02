f06860491edbcd3c75548989624d59d679d5c590
Tr0n76 <richard@schraudt.de>
2016-07-02 13:23:12 +0200

    Harvester carry even more.

--- controller.creeps.js
+++ controller.creeps.js
@@ -93,7 +93,7 @@ function create() {
 	
 	clean();
 	
-	if (createCreep('harvester', 10, [WORK, WORK, WORK, CARRY, CARRY, MOVE])){
+	if (createCreep('harvester', 10, [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE])){
 		return;
 	}
 	if(createCreep('upgrader', 4, [WORK, WORK, WORK, CARRY, CARRY, MOVE])){
