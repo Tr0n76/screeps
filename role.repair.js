f8a0dc34699b9dbb3a111a1b5416ff9e160c4e7c
Tr0n76 <richard@schraudt.de>
2016-07-02 12:24:11 +0200

    Removed log

--- role.repair.js
+++ role.repair.js
@@ -3,9 +3,7 @@ var roleRepair = {
 	run : function(creep) {
 
 		var structures = creep.room.find(FIND_MY_STRUCTURES);		
-		
-		console.log(structures);
-		
+					
 		for (var i=0;i<structures.length;i++){
 			var item = structures[i];
 			if (item.hits<item.hitsMax){
