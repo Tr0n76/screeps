13ee2692497f7a76052df754561294c2dcdee279
Tr0n76 <richard@schraudt.de>
2016-07-03 17:27:24 +0200

    Caching the items to repair

--- role.builder.js
+++ role.builder.js
@@ -16,6 +16,9 @@ var roleBuilder = {
             	
     			for (var i=0;i<structures.length;i++){
     				var item = structures[i];	
+    				if (!item){
+    					continue;
+    				}
 
     				if (!creep.pos.isNearTo(item)) {
     					creep.moveTo(item);
@@ -44,17 +47,29 @@ function setBuildFlagForCreep(creep){
 
 }
 
+
 function getTargetsForRepair(creep){
-	return creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-            return (structure.structureType === STRUCTURE_EXTENSION ||
-                    structure.structureType === STRUCTURE_SPAWN ||
-                    structure.structureType === STRUCTURE_TOWER ||
-                    ((structure.structureType ===  STRUCTURE_ROAD) && (structure.hits < (structure.hitsMax/4)))||    
-                    structure.structureType ===  STRUCTURE_WALL ||
-                    structure.structureType ===  STRUCTURE_RAMPART) && (structure.hits < structure.hitsMax);
-        }
-	});
+	for (var i ; i<creep.memory.targetsForRepair.length;i++){
+		if (item[i].hits==item[i].hitsMax){
+			delete creep.memory.targetsForRepair[i];
+		}
+	}
+	
+	
+	if (!creep.memory.targetsForRepair){
+		creep.memory.targetsForRepair = creep.room.find(FIND_STRUCTURES, {
+	        filter: (structure) => {
+	            return (structure.structureType === STRUCTURE_EXTENSION ||
+	                    structure.structureType === STRUCTURE_SPAWN ||
+	                    structure.structureType === STRUCTURE_TOWER ||
+	                    ((structure.structureType ===  STRUCTURE_ROAD) && (structure.hits < (structure.hitsMax/4)))||    
+	                    structure.structureType ===  STRUCTURE_WALL ||
+	                    structure.structureType ===  STRUCTURE_RAMPART) && (structure.hits < structure.hitsMax);
+	        }
+		});
+	}else{
+		return creep.memory.targetsForRepair;
+	}				
 }
 
 module.exports = roleBuilder;
