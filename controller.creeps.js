e7d973b0ad48beaeb9a00a1e206c6aa5a364c35c
Tr0n76 <richard@schraudt.de>
2016-07-04 15:10:53 +0200

    Builder changed

--- controller.creeps.js
+++ controller.creeps.js
@@ -105,7 +105,7 @@ function create(creps) {
 	if(createCreep('upgrader', 2, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE])){
 		return;
 	}
-	if(createCreep('builder', 2, [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE])){
+	if(createCreep('builder', 2, [WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE])){
 		return;
 	}	
 	if(createCreep('guard', 3, [ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE, MOVE, TOUGH, TOUGH, TOUGH, TOUGH])){
